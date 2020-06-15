af1db78b474da41c37e5f9a76588d5634a1f4505
Jameel Hassan <jhassan@Jameels-MacBook-Pro.local>
2020-06-15 16:36:09 +0530

    NN with out sparse vec

--- Yale.m
+++ Yale.m
@@ -1,6 +1,7 @@
 clear all; close all;
 clc;
 
+%%
 data = load("Yale_32x32.mat", 'fea', 'gnd');
 indata = data.fea;
 out = data.gnd;
@@ -12,5 +13,16 @@ Y = Y';
 X = indata';
 
 net = feedforwardnet([10,5]);
-net = configure(net,X,out');
-view(net);
+
+%% Neural Net with output vals ranging 1-15
+% net = configure(net,X,out');
+% view(net);
+% load('nn_1thru15.mat');
+% stem(round(nn(X))); 
+% hold on; 
+% stem(out');
+
+%% Neural Net with output as a 1x15 sparse vector
+net = configure(net,X,Y);
+net.layers{3}.transferFcn = 'logsig';
+view(net)
